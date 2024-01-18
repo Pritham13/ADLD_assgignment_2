f6b0b5c3174ed4c229a04f4b09f52cbb028fd736
Pritham13 <143584964+Pritham13@users.noreply.github.com>
2024-01-18 12:43:45 +0530

    Update fifo.v

--- fifo.v
+++ fifo.v
@@ -1,12 +1,32 @@
 module fifo (
     input [7:0] data_in,
-    input [4:0]ptr_in,
-    input [4:0]ptr_out,
     input en_read,en_write,reset,clk,
     output reg [7:0] data_out
 );
 reg [7:0] register [15:0];
+reg [3:0]ptr_rd,ptr_wr;
 integer i;
+
+always @ (posedge clk)
+begin
+    if (reset)
+    begin
+        ptr_wr<=4'b0;
+    end 
+    if(en_write)
+        ptr_wr<= ptr_wr+1;
+end
+
+always @ (posedge clk)  
+begin
+    if (reset)
+    begin
+        ptr_rd<=4'b0;
+    end 
+    if(en_write)
+        ptr_rd<= ptr_rd+1;
+end
+
 always @(posedge clk) 
 begin
     if (reset)
@@ -16,20 +36,18 @@ begin
             register [i]= 8'd0; 
         end 
         data_out <=8'b0;
+        ptr_wr<=0;
+        ptr_rd<=0;
     end 
     else
     begin
-        // case ({en_write,en_read})
-        // 2'b01: register[ptr_in]<=data_in;
-        // 2'b10: data_out <= register[ptr_out];
-        // endcase 
         if  (en_write)
         begin
-           register[ptr_in]<=data_in;
+           register[ptr_wr]<=data_in;
         end
         if  (en_read)
         begin
-            data_out <= register[ptr_out];
+            data_out <= register[ptr_rd];
         end 
         else 
         begin
