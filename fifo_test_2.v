885c784244f159b19ed0d97739cf15d754459ad3
Pritham13 <prithamhsuvarna@gmail.com>
2024-01-18 20:52:58 +0530

    updating files

--- fifo_test_2.v
+++ fifo_test_2.v
@@ -1,83 +1,58 @@
 module fifo_2 (
     input [7:0] data_in,
-    input en_read,en_write,reset,clk,
-    output reg overflow,underflow,
+    input en_read, en_write, reset, clk,
+    output reg overflow, underflow,
     output reg [7:0] data_out
 );
+
 reg [7:0] register [0:15];
-reg [3:0]ptr_wr;
+reg [3:0] ptr_wr;
 integer i;
-
-// always @ (posedge clk)
-// begin
-//     if (reset)
-//     begin
-//         ptr_wr<=4'b0;
-//     end 
-//     if(en_write)
-//         if(ptr_wr!=4'b0)
-//             ptr_wr<= ptr_wr-1;
-//         else
-//             overflow <=1;
-// end
-
-// always @ (posedge clk)  
-// begin
-//     if (reset)
-//     begin
-//         ptr_rd<=4'b0;
-//     end 
-//     if(en_read)
-//         if(ptr_rd!=4'b0)
-//             ptr_rd<= ptr_rd-1;
-//         else
-//             underflow <=1;
-        
-// end
 
 always @(posedge clk) 
 begin
     if (reset)
     begin
-        for(i =0;i<16;i=i+1)
+        for (i = 0; i < 16; i = i + 1)
         begin
-            register [i]= 8'd0; 
+            register[i] = 8'd0; 
         end 
-        data_out <=8'b0;
-        ptr_wr<=16;
-        overflow<=0;
-        underflow<=0;
+        data_out <= 8'b0;
+        ptr_wr <= 4'd15;
+        overflow <= 0;
+        underflow <= 0;
     end 
     else
     begin
-        if  (en_write)
+        if (en_write)
         begin
-           register[ptr_wr]<=data_in;
-            if(ptr_wr!=4'b0)
-                ptr_wr<= ptr_wr-1;
+            register[ptr_wr] <= data_in;
+            if (ptr_wr != 4'b0)
+                ptr_wr <= ptr_wr - 1;
             else
-                overflow <=1;
+                overflow <= 1;
         end
-        if  (en_read)
+        if (en_read)
         begin
-            if(register[16]!=8'b0)
+            if (register[15] != 8'b0)
             begin
-                data_out <= register[16];
-                for (i=0;i<ptr_wr;i++)
+                data_out <= register[15];
+                for (i = 0; i < ptr_wr; i = i + 1)
                 begin
-                    register[i]<=register[i+1];
-                    ptr_wr<=ptr_wr-1;
+                    register[i] <= register[i + 1];
                 end
+                ptr_wr <= ptr_wr - 1;
+            end
             else
             begin
-                underflow<=1'b1;   
+                underflow <= 1'b1;   
             end
         end
-        // end 
         else 
-            begin
-                data_out<=0;
-            end
+        begin
+            data_out <= 8'b0;
+        end
     end 
 end
+
 endmodule
